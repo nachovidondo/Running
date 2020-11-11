a107568262f9c2dc80f1824d621020581f8c12fd
Santiago Alurralde <santi@emergent-seed.org>
2020-11-11 12:03:57 -0500

    Cambios Formato

--- src/app.py
+++ src/app.py
@@ -2,49 +2,37 @@ from tkinter import *
 from tkinter import messagebox
 from datetime import datetime
 
-ventana=Tk()
+ventana = Tk()
 ventana.title("Programa de Running")
-ventana.config(bg="#D8D8D8")
+ventana.config(bg = "#D8D8D8")
 ventana.geometry("400x300")
 ventana.iconbitmap("runner.ico")
 
-imagenL=PhotoImage(file="imagen1.png")
-labelimagen=Label(ventana,image=imagenL).place(x=0,y=0)
-
-
-
+# imagenL = PhotoImage(file="imagen1.png")
+# labelimagen = Label(ventana,image=imagenL).place(x=0,y=0)
     
 def Enviardatos():
-    textoprint= text1.get( )
-    messagebox.showinfo( "corriste : ",textoprint)
+    textoprint = text1.get()
+    messagebox.showinfo("Corriste : ", textoprint)
     ventana.destroy()
 
+label1 = Label(ventana, text = "¿Cuantos minutos corriste hoy? ")
+label1.place(x = 20,y = 120)
+label1.config(fon = ("lato",10))
+label1.config(bg = "#DCDCDC", fg = "black")
 
-
-label1=Label(ventana,text="¿Cuantos minutos corriste hoy? ")
-label1.place(x=20,y=120)
-label1.config(fon=("lato",10))
-
-label1.config(bg="#DCDCDC",fg="black")
-
-
-text1=Entry(ventana)
+text1 = Entry(ventana)
 text1.pack()
-text1.place(y=120,x=230)
-
+text1.place(y = 120, x = 230)
 
 #botones
-boton_nuevo=Button(ventana,text=" Nuevo Registro ",fon=("lato",10)).place(x=180,y=200)
-
-
-boton_guardar=Button(ventana,text=" Guardar ",fon=("lato",10),command=Enviardatos).place(x=320,y=200)
-
-
+boton_nuevo = Button(ventana, text = " Nuevo Registro ", fon = ("lato",10)).place(x = 180, y = 200)
+boton_guardar = Button(ventana, text = " Guardar ", fon = ("lato", 10), command = Enviardatos).place(x = 320, y = 200)
 
 def fecha_hora():
-    now=datetime.now()
-    messagebox.showinfo("Fecha y Hora",now)
+    now = datetime.now()
+    messagebox.showinfo("Fecha y Hora", now)
 
-botonfecha_hora=Button(ventana,text="Fecha y hora ",command=fecha_hora).place (x=20,y=20)
+botonfecha_hora = Button(ventana,text = "Fecha y hora ",command=fecha_hora).place (x=20,y=20)
 
 ventana.mainloop()
--- src/runing.py
+++ src/runing.py
@@ -1,33 +1,28 @@
 from tkinter import *
 
-ventana=Tk()
+ventana = Tk()
 ventana.title("Programa de Running")
 ventana.geometry("500x400")
 
-
-
-    
-frame=Frame(ventana)
+frame = Frame(ventana)
 frame.pack()
-frame.config(bg="#00CED1",width=300, height=250)
-frame.place(y=10, x=100)
-
+frame.config(bg = "#00CED1",width = 300, height = 250)
+frame.place(y = 10, x = 100)
 
 #etiqueta
-label1=Label(frame,text="Digite la cantidad de minutos que corrio hoy")
-label1.config(bg="#DCDCDC",fg="black")
-label1.place(y=8,x=45)
+label1 = Label(frame,text = "Digite la cantidad de minutos que corrio hoy")
+label1.config(bg = "#DCDCDC",fg = "black")
+label1.place(y = 8,x = 45)
 
 #campo de texto
-text1=Entry(frame)
-text1.place(y=55,x=45)
+text1 = Entry(frame)
+text1.place(y = 55,x = 45)
 
 #botones
+boton_nuevo = Button(frame,text = " Nuevo Registro ")
+boton_nuevo.config(y = 90, x = 55)
 
-boton_nuevo=Button(frame,text=" Nuevo Registro ")
-boton_nuevo.config(y=90, x=55)
-
-boton_guardar=Button(frame,text=" Guardar ")
-boton_guardar.config(y=90, x=35)
+boton_guardar = Button(frame,text = " Guardar ")
+boton_guardar.config(y = 90, x = 35)
 
 ventana.mainloop()
