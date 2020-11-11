42e4c5b8c83cbf1c56574cc281995a7d12d80cad
NachoVidondo <ignaciovidondo@hotmail.com>
2020-11-10 21:56:40 -0300

    Boton Fecha y Hora

--- src/app.py
+++ src/app.py
@@ -12,8 +12,7 @@ imagenL=PhotoImage(file="imagen1.png")
 labelimagen=Label(ventana,image=imagenL).place(x=0,y=0)
 
 
-def nuevo():
-    text1.config (state='normal')
+
     
 def Enviardatos():
     textoprint= text1.get( )
@@ -22,16 +21,11 @@ def Enviardatos():
 
 
 
-
-
-
 label1=Label(ventana,text="¿Cuantos minutos corriste hoy? ")
 label1.place(x=20,y=120)
 label1.config(fon=("lato",10))
 
 label1.config(bg="#DCDCDC",fg="black")
-
-
 
 
 text1=Entry(ventana)
@@ -45,5 +39,12 @@ boton_nuevo=Button(ventana,text=" Nuevo Registro ",fon=("lato",10)).place(x=180,
 
 boton_guardar=Button(ventana,text=" Guardar ",fon=("lato",10),command=Enviardatos).place(x=320,y=200)
 
+
+
+def fecha_hora():
+    now=datetime.now()
+    messagebox.showinfo("Fecha y Hora",now)
+
+botonfecha_hora=Button(ventana,text="Fecha y hora ",command=fecha_hora).place (x=20,y=20)
 
 ventana.mainloop()
