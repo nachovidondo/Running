9932a188f885d69f4d72f0056725862e337a1763
Santiago Alurralde <santi@emergent-seed.org>
2020-11-12 16:23:19 -0500

    Clase DatabaseConnection;

--- src/consulta.py
+++ src/consulta.py
@@ -2,27 +2,15 @@ import sqlite3
 import os
    
 APP_PATH = os.getcwd()
-DB_PATH = APP_PATH+ '/my_database.db'
+DB_PATH = APP_PATH + '/my_database.db'
 
-con = None 
-cursor = None
+class DBConnection:
+    def __init__(self):
+        self.con = sqlite3.connect(DB_PATH)
+        self.cursor = self.con.cursor()
 
-def connect():
-    con = sqlite3.connect(DB_PATH)
-    cursor = con.cursor()
-    print('debug connect', con, cursor.execute)
-
-    return cursor
-
-def insertData(saveTime, runningTime):
-    saveTime = str(saveTime)
-    print('debug insert', con, cursor.execute)
-    con = sqlite3.connect(DB_PATH)
-    cursor = con.cursor()
-
-    # cursor.execute( 
-    #     """INSERT INTO TIEMPO_CORRIDO VALUES (?, ?)""", (saveTime, runningTime))
-    # con.commit()
-    
-       
-#con.close()
+    def insertData(self, saveTime, runningTime):
+        print('debug insert', self.con, self.cursor)
+        self.cursor.execute("""INSERT INTO TIEMPO_CORRIDO VALUES (?, ?)""", (saveTime, runningTime))
+        self.con.commit()
+        self.con.close()
